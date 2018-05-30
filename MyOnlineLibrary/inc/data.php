fca3f047393f45d2265e5d3b1e3e535beb086637
jhnober212 <39781553+jhnober212@users.noreply.github.com>
2018-05-30 19:10:16 -0400

    Add files via upload

---
+++ MyOnlineLibrary/inc/data.php
@@ -0,0 +1,182 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+    "title" => "The Flavor Bible",
+    "img" => "img/media/FB.png",
+   "genre" => "Cookbook",
+   "format" => "Hardcover",
+   "year" => 2008,
+   "category" => "Books",
+   "authors" => [
+       "Karen A. Page",
+       "Andrew Dornenburg",
+
+   ],
+   "publisher" => "Little, Brown and Company",
+   "isbn" => '978-0316118408
+'
+];
+
+$catalog[102] = [
+    "title" => "Power of Habit",
+    "img" => "img/media/PoH.png",
+   "genre" => "Self-help",
+   "format" => "Paperback",
+   "year" => 2014,
+   "category" => "Books",
+   "authors" => [
+       "Charles Duhigg",
+
+   ],
+   "publisher" => "Random House Trade Paperbacks",
+   "isbn" => '978-0812981605'
+];
+
+$catalog[103] = [
+    "title" => "The Life-Changing Magic of Tidying Up: The Japanese Art of Decluttering and Organizing",
+    "img" => "img/media/KOnmari.png",
+   "genre" => "Self-help",
+   "format" => "Paperback",
+   "year" => 2014,
+   "category" => "Books",
+   "authors" => [
+       "Marie Kondo",
+   ],
+   "publisher" => "Ten Speed Press",
+   "isbn" => '978-1607747307'
+];
+
+$catalog[104] = [
+    "title" => "The Alchemist",
+    "img" => "img/media/Alchemist.png",
+   "genre" => "Fiction",
+   "format" => "Paperback",
+   "year" => 1993,
+   "category" => "Books",
+   "authors" => [
+       "Paulo Coelho",
+   ],
+   "publisher" => "HarperOne; 1st edition",
+   "isbn" => '978-0061122415'
+];
+
+//Movies
+$catalog[201] = [
+   "title" => "Get Out",
+   "img" => "img/media/Getout.png",
+   "genre" => "Psycho Thriller",
+   "format" => "Blu-Ray",
+   "year" => 2017,
+   "category" => "Movies",
+   "director" => "Jordan Peele",
+   "writers" => [
+       "Jordan Peele"
+
+   ],
+   "stars" => [
+       "Daniel Kaluuya",
+       "Allison Williams",
+       "Bradley Whitford"
+   ]
+];
+
+$catalog[202] = [
+   "title" => "Coco",
+   "img" => "img/media/coco.png",
+   "genre" => "Slice of life, Animation, Drama, Fantasy",
+   "format" => "Blu-Ray",
+   "year" => 2017,
+   "category" => "Movies",
+   "director" => "Lee Unkrich",
+   "writers" => [
+       "Lee Unkrich"
+
+   ],
+   "stars" => [
+       "Gael Garcia Bernal",
+       "Benjamin Bratt",
+       "Renee Victor"
+   ]
+];
+
+$catalog[203] = [
+   "title" => "Your Name",
+   "img" => "img/media/Yourname.png",
+   "genre" => "Animation, Drama, Fantasy",
+   "format" => "Blu-Ray",
+   "year" => 2017,
+   "category" => "Movies",
+   "director" => "Makoto Shinkai",
+   "writers" => [
+       "Makoto Shinkai"
+
+   ],
+   "stars" => [
+       "Miki Okudera",
+       "Mitsuha Miyamizu",
+       "Tsukasa Fuji"
+   ]
+];
+
+$catalog[204] = [
+   "title" => "The girl who lept through time",
+   "img" => "img/media/Tokikake.png",
+   "genre" => "Animation, Drama, Fantasy",
+   "format" => "Blu-Ray",
+   "year" => 2017,
+   "category" => "Movies",
+   "director" => "Mamoru Hosoda",
+   "writers" => [
+       "Mamoru Hosoda"
+
+   ],
+   "stars" => [
+       "Riisa Naka",
+       "Takuya Ishida",
+       "Mitsutaka Itakura"
+   ]
+];
+
+//Music
+$catalog[301] = [
+   "title" => "Oddloop",
+   "img" => "img/media/Frederic.png",
+   "genre" => "J-Rock",
+   "format" => "CD",
+   "year" => 2014,
+   "category" => "Music",
+   "artist" => "Frederic"
+];
+
+$catalog[302] = [
+   "title" => "Σ (Sigma)",
+   "img" => "img/media/Reol.png",
+   "genre" => "EDM",
+   "format" => "CD",
+   "year" => 2015,
+   "category" => "Music",
+   "artist" => "REOL"
+];
+
+
+$catalog[303] = [
+   "title" => "Kiss of Death",
+   "img" => "img/media/KoD.png",
+   "genre" => "J-Pop",
+   "format" => "CD",
+   "year" => 2018,
+   "category" => "Music",
+   "artist" => "Mika Nakashima"
+];
+
+$catalog[304] = [
+   "title" => "Watashi igai watashi janai no",
+   "img" => "img/media/Watashi.png",
+   "genre" => "J-Rock",
+   "format" => "CD",
+   "year" => 2016,
+   "category" => "Music",
+   "artist" => "Gesu no Kiwami Otome"
+];
+?>
---
+++ MyOnlineLibrary/inc/footer.php
@@ -0,0 +1,14 @@
+</div>
+
+<div class="footer">
+<div class="wrapper">
+<ul>
+  <li><a href="https://www.instagram.com/LagGuardiaCC/">Twitter</a></li>
+  <li><a href="https://www.facebook.com/LaGuardiaCC">Faebook</a></li>
+</ul>
+
+<p>&copy;<?php echo date("Y"); ?> 2018 My Online Library Collection </p>
+</div>
+</div>
+</body>
+</html>
